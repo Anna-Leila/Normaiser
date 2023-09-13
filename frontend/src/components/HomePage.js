d3b62d9448734ae1fc904d44f366f323d264d170
Anna <annzoykina@gmail.com>
2023-09-13 18:31:35 +0500

    Shining text added to homepage, other small css changes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,14 @@
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
+import "./homepage.css";
 
 function HomePage() {
+    document.body.style.backgroundColor = "rgba(100,255,117,0.7)"
+
     return <>
-        <h1>Home page here!</h1>
-        <Link to="/normaliser" className="btn btn-primary">Get to norm!</Link>
+        <div id="homepage">
+            <div className="pretty-heading">Normaiser</div>
+            <Link to="/normaliser" className="btn btn-primary">Get to norm!</Link>
+        </div>
     </>
 }
 
